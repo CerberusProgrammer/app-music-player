11399d5d5e5b3531cefe80b4bb823afa1409f7a1
Cerberus <cerberusprogrammer@gmail.com>
2022-07-11 14:57:24 -0700

    Later i will send more progress

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {app, BrowserWindow, ipcMain, dialog} = require('electron');
 const path = require('path');
 var jsmediatags = require('jsmediatags');
+const fs = require('fs')
 
 var file = null;
 var tags = null;
@@ -28,7 +29,7 @@ async function handleFileOpen() {
   if (canceled) {
     return
   } else {
-
+    writeNow(filePaths[0])
     return filePaths[0]
   }
 }
@@ -37,12 +38,19 @@ async function handleChangeImage() {
   return tags
 }
 
+function writeNow (filePath) {
+  fs.writeFile('test.txt', filePath, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+  })
+}
+
 function createWindow () {
   const mainWindow = new BrowserWindow({
     webPreferences: {
       autoHideMenuBar: true,
-      width: 1500,
-      height: 720,
       webPreferences: {
         nodeIntegration: true,
         contextIsolation: false,
@@ -52,9 +60,9 @@ function createWindow () {
     }
   })
   
+  mainWindow.setSize(400, 600)
   mainWindow.setMenuBarVisibility(false)
   mainWindow.loadFile('src/index.html')
-  mainWindow.openDevTools()
 }
 
 app.whenReady().then(() => {
